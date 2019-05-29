8b3e1c2da6a52b91e3a8be85d90c0a04b6815a66
Stefano Valentini <valentini.stefano@outlook.com>
2019-05-29 21:05:53 +0200

    refactored vuejs project structure

--- code/webclient/src/App.vue
+++ code/webclient/src/App.vue
@@ -8,13 +8,8 @@
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script src="./App.js"></script>
+
+<style src="./App.css"></style>
+
+
